00277cfd6cac1da226b4598f114f06fff8b4d098
Gabrielle Jonathan Juanillo <gcjuanillo@student.apc.edu.ph>
2023-01-15 22:50:47 +0800

    JS Lesson 2 and 3: Introduction and Where to

---
+++ week5/myScript.js
@@ -0,0 +1,8 @@
+function secondPage() {
+    document.getElementById("intro").innerHTML = "I graduated Senior High School in Treston International College Located at BGC, Taguig City. I'm currently a sophomore in Asia Pacific College taking Bachelor of Science in Computer Science specializing in Software and Systems. My goal in life is to finish my course and be able to work abroad while alsohaving a sidejob as a Music Producer.";
+    
+}
+
+function thirdPage() {
+    document.getElementById("intro").innerHTML = "I always wanted to pursue a career in the music industry but chose Computer Science because Music Production has a lot of risks to take and takes a lot of time to have a decent career in the field. If you want to know more about me, just navigate through this page!";
+}
